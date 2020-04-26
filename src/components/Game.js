8fd8673c48a1180e75878b6ebc3e32a354f8fe5d
jequezinho <jroverby92@gmail.com>
2020-04-26 16:18:17 -0500

    Done.

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Pancake from './Pancake';
 
-class Game extends React.Component {
+export default class Game extends React.Component {
 
   constructor(props) {
     super(props);
@@ -17,6 +17,10 @@ class Game extends React.Component {
   }
 
   // TODO: create a componentDidMount() which will set the current time
+
+  componentDidMount() {
+    this.setCurrentTime()
+  }
   
   setCurrentTime = () => {
     this.setState({ time: new Date(Date.now())});
@@ -62,5 +66,3 @@ class Game extends React.Component {
     )
   }
 }
-
-export default Game;
